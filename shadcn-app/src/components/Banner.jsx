aa3fc9507d5ca06098b2741b92990f98c23bfe18
Armond-Guze <Armond.Guze@yahoo.com>
2024-07-15 23:07:58 -0400

    implement navbar

---
+++ shadcn-app/src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className='w-full nav-height relative'>
+      banner
+      <div className='h-5/6 w-full flex flex-col'>
+      <h1>hellofg</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Banner
