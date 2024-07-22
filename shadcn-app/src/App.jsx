1d89398236196948f89039989482314d0223ce80
Armond-Guze <Armond.Guze@yahoo.com>
2024-07-22 03:19:53 -0400

    create components, constants page

--- shadcn-app/src/App.jsx
+++ shadcn-app/src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
+import styles from "./style.js";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components"
 
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Banner />
-
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}><Navbar /></div>
     </div>
-  )
-}
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
--- shadcn-app/src/components/Navbar.jsx
+++ shadcn-app/src/components/Navbar.jsx
@@ -1,21 +1,9 @@
+import React from 'react'
+
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center bg-black color-white">
-      <nav className="flex w-full screen-max-width nav-black">
-        <img src="" alt="ag logo" />
-        <div className="flex flex-1 justify-center">
-          <ul className="flex space-x-5">
-            <li className="hover:text-white transition-all cursor-pointer">Home</li>
-            <li className="hover:text-white transition-all cursor-pointer">About Us</li>
-            <li className="hover:text-white transition-all cursor-pointer">Our Services</li>
-          </ul>
-        </div>
-        <div>
-        <button className='rounded bg-blue-200 btn'>Contact Us</button>
-        </div>
-      </nav>
-    </header>
-  );
-};
+    <div>Navbar</div>
+  )
+}
 
-export default Navbar;
+export default Navbar
