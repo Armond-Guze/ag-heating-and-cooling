1c3339227560458044e1289b5ff0c3951ed4577d
Armond-Guze <Armond.Guze@yahoo.com>
2024-07-22 05:08:39 -0400

    implement hero section, getStarted

--- shadcn-app/src/App.jsx
+++ shadcn-app/src/App.jsx
@@ -1,10 +1,12 @@
-import styles from "./style.js";
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components"
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}><Navbar /></div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
@@ -12,8 +14,8 @@ const App = () => (
         <Hero />
       </div>
     </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
