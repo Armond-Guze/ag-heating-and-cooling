c01ad7df60cb6701cb6326ae1d6eb3c366779f61
Armond-Guze <Armond.Guze@yahoo.com>
2024-07-22 23:19:20 -0400

    start cardDeal, complete billing section

--- shadcn-app/src/components/CardDeal.jsx
+++ shadcn-app/src/components/CardDeal.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card Deal <br className='sm:block hidden'/> in a few easy steps</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda necessitatibus nostrum voluptates? Rem, placeat voluptate. Suscipit dicta fugit mollitia autem? Laboriosam a omnis aut numquam laborum. Saepe nemo ea repellat!</p>
+      </div>
+      
+     </section>
   )
 }
 
