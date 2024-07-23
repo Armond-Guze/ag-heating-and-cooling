b93346d5f483d7f2f9d7e4f8b5857a1ef83daa2a
Armond-Guze <Armond.Guze@yahoo.com>
2024-07-22 23:54:00 -0400

    implement testimonials

--- shadcn-app/src/components/CardDeal.jsx
+++ shadcn-app/src/components/CardDeal.jsx
@@ -8,6 +8,11 @@ const CardDeal = () => {
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>Find a better card Deal <br className='sm:block hidden'/> in a few easy steps</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda necessitatibus nostrum voluptates? Rem, placeat voluptate. Suscipit dicta fugit mollitia autem? Laboriosam a omnis aut numquam laborum. Saepe nemo ea repellat!</p>
+      <Button styles="mt-10"/>
+      </div>
+
+      <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]' />
       </div>
       
      </section>
